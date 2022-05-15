b3730379786e0dedf3faa3ea4628a0d9aa95a250
Afroza2 <afroza.nowshin@gmail.com>
2022-05-15 18:10:42 +0600

    front end filter for stocks

--- src/pages/Stocks.js
+++ src/pages/Stocks.js
@@ -1,32 +1,34 @@
-import React, { useState, useEffect } from 'react';
-import { AgGridReact, AgGridColumn } from 'ag-grid-react';
+import React, { useState, useEffect} from 'react';
+import { AgGridReact} from 'ag-grid-react';
 import axios from "axios";
 
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
-console.log(process.env.REACT_APP_API_KEY_FMP)
-
-const stock_api_key = process.env.REACT_APP_API_KEY_FMP
 
 //https://financialmodelingprep.com/api/v3/nasdaq_constituent?apikey=b36cde29d2d36444523763a08e9d8869
 
+const url = `https://financialmodelingprep.com/api/v3/nasdaq_constituent?apikey=b36cde29d2d36444523763a08e9d8869`
 
 function Stock() {
     const [stockData, setStockData] = useState([])
     useEffect(() => {
-        axios.get(`https://financialmodelingprep.com/api/v3/nasdaq_constituent?apikey=${stock_api_key}`)
+        axios.get(url)
             .then((response) => {
                 setStockData(response.data)
             })
     }, [])
 
+    const columns = [
+    { headerName: "Symbol", field: "symbol",filter: true },
+    { headerName: "Name", field: "name" },
+    { headerName: "Industry", field: "sector", filter: true },
+  ];
+
   return (
    <div className="ag-theme-alpine" style={{ height: 400, width: 600 }}>
-       <AgGridReact  rowData={stockData}>
-           <AgGridColumn field="symbol" headerName="Symbol" cellClass="vertical-middle" />
-          <AgGridColumn field="name" headerName="Name" cellClass="vertical-middle" />
-          <AgGridColumn field="sector" headerName="Industry"  cellClass="vertical-middle" />
-       </AgGridReact>
+
+       <AgGridReact  rowData={stockData} columnDefs={columns}/>
+
     </div>
   );
 }
